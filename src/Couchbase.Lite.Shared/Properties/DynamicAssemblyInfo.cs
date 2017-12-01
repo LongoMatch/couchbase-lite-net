3757f11a6b61dd99973a70eb93c77ec20eb9f5df
Andoni Morales Alastruey <ylatuya@gmail.com>
2017-12-01 13:55:17 +0100

    Fix assembly version
    This fixes a link issue in iOS where some dependencies are
    against the 1.3.1 version and others against the 0.0.0 one

--- src/Couchbase.Lite.Shared/Properties/DynamicAssemblyInfo.cs
+++ src/Couchbase.Lite.Shared/Properties/DynamicAssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Reflection;
 
 
-[assembly: AssemblyVersion ("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0")] // Used by nuget for semver detection.
+[assembly: AssemblyVersion ("1.3.1.0")]
+[assembly: AssemblyInformationalVersion("1.3.1")] // Used by nuget for semver detection.
